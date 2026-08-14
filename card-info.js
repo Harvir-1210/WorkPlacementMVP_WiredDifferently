47302d008357746868eb147c1d595b9ed543e6d8
Harvir-1210 <harvir.brainch@gmail.com>
2026-08-14 13:51:40 +0100

    Update card-info.js

--- card-info.js
+++ card-info.js
@@ -3,6 +3,7 @@ const cards = JSON.parse(localStorage.getItem("flashCards")) || [];
 const card = cards[Number(selectedCard)];
 
 document.getElementById("card-title").textContent = `Flash Card ${Number(selectedCard) + 1}`;
+document.getElementById("trait").textContent = card.trait;
 document.getElementById("type").textContent = card.type;
 document.getElementById("explanation").textContent = card.explanation;
 
